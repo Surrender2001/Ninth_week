c2a471fae89471cf287abb5998c330d8ec54eb09
Sergey <shesergey2001@gmail.com>
2021-04-09 11:47:56 +1000

    100%

--- ninth_week/Source.cpp
+++ ninth_week/Source.cpp
@@ -5,6 +5,7 @@
 #include<sstream>
 #include<vector>
 #include<map>
+#include"QueueP.h"
 using namespace std;
 
 
@@ -21,7 +22,6 @@ int main()
 	{
 		cout << "Файл открыт" << endl;
 		getline(fin, str);
-		//cout << str << endl;
 	}
 	fin.close();
 
@@ -40,12 +40,46 @@ int main()
 		//cout << word << '\n';//выборка слов
 	}
 
-	priority_queue<string,vector<string>> q;
-	for (auto str: words)
+	int n = 1;
+	map<string, int> slovar;
+	priority_queue<string> q;
+
+	for (auto word:words)
 	{
-		cout << str << " ";
+		cout << word << " ";
+	}
+	cout << endl;
+
+	for (int i=0;i<words.size()-1;i++)
+	{
+		for (int j = i+1; j < words.size(); j++)
+		{
+			if (words[i] == words[j])
+			{
+				n++;
+			}
+		}
+
+		cout <<words[i]<<"\tn= " << n << endl;		
+		auto it = slovar.find(words[i]);
+		if (it == slovar.end())
+		{
+			slovar.emplace(words[i], n);
+		}
+		n = 1;
 	}
 
+	for (auto& item : slovar)
+	{
+		cout << item.first << " : " << item.second << endl;
+	}
+	QueueP<string> Q1;
+	
+	for (auto& item : slovar)
+	{
+		Q1.Push(item.first,item.second);
+	}
 
+	Q1.Print("Q1");
 	return 0;
 }
